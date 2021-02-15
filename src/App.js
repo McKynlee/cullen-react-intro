a2696f56ba9649cc3f11ff6e121b3690aa827f6d
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-15 15:39:36 -0600

    My first react code!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// If this were node we'd:
+// const HelloCullen = require('./HelloCullen');
+// But client-side (React) we do this:
+import HelloCullen from './HelloCullen';
+
+// React is made up of components
+// App is a component.
+// Components are functions that return JSX
+// You can nest components (see HelloCullen below)
 function App() {
   return (
     <div className="App">
+      <HelloCullen />
+      <HelloCullen />
       <header className="App-header">
+        <HelloCullen />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,5 +33,10 @@ function App() {
     </div>
   );
 }
+
+// Moved to HelloCullen module
+// function HelloCullen() {
+//   return <h1>Hello, Cullen!!</h1>;
+// }
 
 export default App;
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,8 @@
+function HelloCullen() {
+  return <h1>Hello, Cullen!!</h1>;
+}
+
+// In node, we'd: module.exports = HelloCullen
+
+// Client-side (React) we do this:
+export default HelloCullen;
