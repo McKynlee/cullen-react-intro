c4a9e702cbf7e5d3e487e975bcbbb6bf5885a079
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-15 16:06:31 -0600

    Made a happy hour drink count

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import useState from React to render variables:
+// This syntax is called Object Destructuring
+// This is same as: import React from 'react';
+// const useState = React.useState;
+import { useState } from 'react';
+// useState is a hook, which is a fancy thing to do stuff in react
+// all hooks start with use (usually means you're tracking value
+// of something across multiple calls of a function)
+
 // If this were node we'd:
 // const HelloCullen = require('./HelloCullen');
 // But client-side (React) we do this:
@@ -11,25 +20,34 @@ import HelloCullen from './HelloCullen';
 // Components are functions that return JSX
 // You can nest components (see HelloCullen below)
 function App() {
+  let name = 'Edan';
+
+  //THIS WON"T RENDER:
+  // let drinkCount = 1;
+  // So instead us state to define two new variables, starting at 1
+  const [drinkCount, setDrinkCount] = useState(1);
+  // What this is made up of = we are creating an array:
+  // const returnValOfUseState = useState(1);
+  // const drinkCount = returnValOfUseState[0];
+  // const setDrinkCount = returnValOfUseState[1];
+
   return (
-    <div className="App">
-      <HelloCullen />
-      <HelloCullen />
-      <header className="App-header">
-        <HelloCullen />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cullen Happy Hour!</h1>
+      <p>I have had {drinkCount} drink</p>
+      <p>{name} has had 1 drink</p>
+
+      {/* Write event listener as an attribute! */}
+      <button
+        onClick={() => {
+          setDrinkCount(drinkCount + 1);
+          console.log('Drink up!');
+        }}
+      >
+        Drink Up ^
+      </button>
+
+      <button onClick={() => setDrinkCount(0)}>Sober Up</button>
     </div>
   );
 }
